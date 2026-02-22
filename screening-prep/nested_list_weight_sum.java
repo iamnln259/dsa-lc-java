f116f7ca35391f5adda533b10de67442d0af5812
iamnln259 <sitaram.sf2@gmail.com>
2026-02-22 22:22:04 +0530

    Update nested_list_weight_sum.java

--- screening-prep/nested_list_weight_sum.java
+++ screening-prep/nested_list_weight_sum.java
@@ -1,2 +1,23 @@
 //https://leetcode.com/problems/nested-list-weight-sum/description/
+class Solution{
+	public int depthSum(List<NestedInteger> nestedList){
+		return dfs(nestedList,1);
+	}
 
+	private int dfs(List<NestedIntege> list, int depth){
+		int sum = 0 ;
+		for(NestedInteger ni : list){
+			if(ni.isInteger()){
+			sum += ni.getInteger() * depth ;
+			}
+			else{
+				sum += dfs(ni.getList(), depth +1);
+			}
+		}
+		return sum;
+	}
+
+}
+
+//TC : O(N)
+//SC : O(depth)
